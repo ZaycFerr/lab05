1d7e8e828ee8a97dad62b302eaacb4429763e1c0
ZaycFerr <aglaya.zayc@mail.ru>
2026-05-17 21:24:14 +0100

    lab05: added GTest and tests

---
+++ print.hpp
@@ -0,0 +1,11 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void print(const std::string& text, std::ostream& out);
+void print(const std::string& text, std::ofstream& out);
+
+#endif
---
+++ tests/test1.cpp
@@ -0,0 +1,24 @@
+#include "../print.hpp"
+#include <gtest/gtest.h>
+#include <fstream>
+#include <string>
+
+TEST(Print, InFileStream) {
+    std::string filepath = "file.txt";
+    std::string text = "hello";
+    std::ofstream out{filepath};
+    
+    print(text, out);
+    out.close();
+    
+    std::string result;
+    std::ifstream in{filepath};
+    in >> result;
+    
+    EXPECT_EQ(result, text);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
